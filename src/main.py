51f021d2b68a1541bb08c4bbe828f6a0e024efb8
Opu Zaman <opu.zaman@shibumi.com>
2021-11-10 09:31:40 -0500

    init commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request
+
+application = Flask(__name__)
+
+@application.route("/")
+def home():
+    return render_template("home.html")
+
+@application.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+@application.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
